a08d2cf58093036d8be4cf6d76cc2fb4198ec148
nancel <nestor.ancel@gmail.com>
2014-08-07 11:43:21 -0300

    init

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express')
+, logfmt = require("logfmt")
+, sio = require('socket.io');
+
+var app = express();
+
+app.set('views', __dirname + '/views')
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+  res.render('index');
+});
+
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
